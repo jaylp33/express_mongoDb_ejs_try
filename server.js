4881b3310df575bb9f9e20c520e28fcc0d62ce7a
jaylp33 <jaylp33@gmail.com>
2020-06-11 23:27:18 +1000

    inital projeect

---
+++ server.js
@@ -0,0 +1,29 @@
+// Using dotenv to get the DB location
+if (process.env.NODE_ENV !== "production") {
+	require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+//  set up mongodb
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { dbName:'fullStackPractice', useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+// If error console.log error
+db.on("error", (error) => console.log(error));
+// if connected clg the successful info
+db.on("open", () => console.log("connected to mongoose"));
+
+app.use("/", indexRouter);
+
+app.listen(process.env.PORT || 9900);
